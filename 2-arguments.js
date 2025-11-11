fbdd70b9b5ba52fc51dc55d538741c600d89ca6b
daveodon <odoyodavidotieno653@gmail.com>
2025-11-11 18:55:12 +0300

    project 3

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+let answer =prompt("what is your favorite colour?");
+if (answer == yellow){
+    console.log("No Argument ");
+}
+else if (answer == blue){
+    console.log("Argument found");
+}
+else {
+    console.log("Argument found");
+}
