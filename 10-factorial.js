7d9f4bbeb2936c6c96ef4080601b17132afaa4cc
daveodon <odoyodavidotieno653@gmail.com>
2025-11-12 13:58:39 +0300

    10

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,10 +1,21 @@
-const computeFactorial = (n) => {
-  if (isNaN(n)) return 1;
-  n = parseInt(n, 10);
-  if (n <= 1) return 1;
-  return n * computeFactorial(n - 1);
-};
+#!/usr/bin/node
 
-const args = process.argv.slice(2);
-const input = args[0] || 0;
-console.log(computeFactorial(input));
+function factorial(n) {
+    if (isNaN(n)) {
+        return 1;
+    }
+    n = parseInt(n);
+    if (n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+function main() {
+    const args = process.argv.slice(2);
+    const input = args[0];
+    const result = factorial(input);
+    console.log(result);
+}
+
+main();
