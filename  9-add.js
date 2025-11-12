d4622f392543dd0c80a62fcbdc6373d0c9902a36
daveodon <odoyodavidotieno653@gmail.com>
2025-11-12 13:23:58 +0300

    9

---
+++  9-add.js
@@ -0,0 +1,7 @@
+function add(a,b) {
+    return a + b;
+}
+let words =process.argv.slice(2);
+let numeric1=parseInt(words[0],10);
+let numeric2=parseInt(words[1],10);
+console.log(add(numeric1,numeric2));
